bd02c58d6eecf7cea2704710fa3e2b3170bbbc6d
Edward Nguyen <nhanna@northstudio.vn>
2024-12-12 12:28:37 +0700

    :sparkles: page image preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,37 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type {Metadata} from 'next';
+import {Geist, Geist_Mono} from 'next/font/google';
+import './globals.css';
 import {AppConfig} from '@/config/app.config';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
   title: `${AppConfig.name}'s Profile`,
   description: `${AppConfig.name}'s online profile`,
+  openGraph: {
+    images: AppConfig.avatar,
+  },
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport"
-              content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,shrink-to-fit=no,viewport-fit=cover"/>
-      </head>
+    <head>
+      <meta name="viewport"
+            content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,shrink-to-fit=no,viewport-fit=cover"/>
+    </head>
     <body
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
